ec18c5c8f25393ed2b02be0b25114862ed8a0b6a
danielmhair <danielmhair@gmail.com>
2014-12-06 13:25:22 -0700

    Basic Structure
    Here is the basic structure. I am still working out the naming and css
    styling, but it should give us a start.

---
+++ BYDo.js
@@ -0,0 +1,37 @@
+var jsonFile = [];
+createDynamicList(var listName) {
+	//Create the dynamic list according to the name of the list
+}
+
+
+//BELOW SHOULD PROBABLY MORE SIMPLE THROUGH JQUERY -- THIS WAS OLD CODE I HAD THAT WORKS
+//sorting algorithm ASC
+function sortByKeyAsc(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; 
+		var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+//sorting algorithm DESC
+function sortByKeyDesc(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; 
+		var y = b[key];
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+    });
+}
+
+//Sorting by Due Date
+function sortDue() {
+  jsonFile.listName = sortByKeyAsc(jsonFile.listName, 'due');
+  //Update tables with sorted info using DOM  
+  createDynamicList(jsonFile);
+}
+
+//Sorting by Priority
+function sortPriority() {
+  jsonFile.listName = sortByKeyDesc(jsonFile.listName, 'priority');
+  //Update tables with sorted info using DOM
+  createDynamicList(jsonFile);
+}
