10ef2e2ac6b43e8085a0e2f086e0f78d00e559c5
thuey <thuey100@gmail.com>
2014-12-06 20:34:49 -0700

    added JSON data

---
+++ data.js
@@ -0,0 +1,44 @@
+[
+	{
+		"name": "Final Project",
+		"due": 1418353200,
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "Teacher Evaluation",
+		"due": 1418526000,
+		"priority": "Low",
+		"list": "CS256"
+	},
+	{
+		"name": "Lab 11",
+		"due": 1418356800,
+		"priority": "High",
+		"list": "CS465"
+	},
+	{
+		"name": "Buy Milk",
+		"due": 1418230800,
+		"priority": "Medium",
+		"list": "Grocery List"
+	},
+	{
+		"name": "Buy Bread",
+		"due": 1418230800,
+		"priority": "Medium",
+		"list": "Grocery List"
+	},
+	{
+		"name": "Call the Doctor",
+		"due": 1418317200,
+		"priority": "High",
+		"list": "General"
+	},
+	{
+		"name": "Make Dinner",
+		"due": 1418319900,
+		"priority": "Medium",
+		"list": "General"
+	}
+]
