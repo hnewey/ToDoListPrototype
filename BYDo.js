7e051916fef500af97762ccdebf3a59550a07208
Taylor Bell <taylorcgbell@gmail.com>
2014-12-07 02:39:37 -0700

    Weekly View works, still needs style
    Weekly View works.  Styling is not implemented yet, the table cells
    jump around when the week is changed.

--- BYDo.js
+++ BYDo.js
@@ -1,4 +1,7 @@
 var filteredData = [];
+var currentWeeklyViewDate;
+var monthsOfTheYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 $(document).ready(function() {
   showListTab();
@@ -69,4 +72,96 @@ function sortPriority() {
   jsonFile.listName = sortByKeyDesc(jsonFile.listName, 'priority');
   //Update tables with sorted info using DOM
   createDynamicList(jsonFile);
+}
+
+function weekView() {
+
+  currentWeeklyViewDate = new Date();
+  currentWeeklyViewDate.setHours(0);
+  currentWeeklyViewDate.setMinutes(00);
+  currentWeeklyViewMonth = currentWeeklyViewDate.getMonth();
+
+  renderWeeklyTable();
+}
+
+function nextWeekView() {
+  currentWeeklyViewDate.setDate(currentWeeklyViewDate.getDate() + 7);
+  renderWeeklyTable();
+}
+
+function lastWeekView() {
+  currentWeeklyViewDate.setDate(currentWeeklyViewDate.getDate() - 7);
+  renderWeeklyTable();
+}
+
+function renderWeeklyTable() {
+  var weekViewHtml = '';
+  $('#weekly-header-bar').html('');
+
+  var lastDateOfWeek = new Date(currentWeeklyViewDate.getFullYear(), currentWeeklyViewDate.getMonth(), currentWeeklyViewDate.getDate(), 23, 59, 0);
+  lastDateOfWeek.setDate(currentWeeklyViewDate.getDate() + 6);
+
+  var mm = monthsOfTheYear[currentWeeklyViewDate.getMonth()]; //January is 0!
+  var lastDayMm = monthsOfTheYear[lastDateOfWeek.getMonth()];
+  var yyyy = currentWeeklyViewDate.getFullYear();
+  var lastDayYyyy = lastDateOfWeek.getFullYear();
+  
+  //Sets correct dates in the weekly view header bar
+  weekViewHtml += '<p id="weekly-header-paragraph">';
+  weekViewHtml += '<button id="left-arrow-weekly-bar" onclick="lastWeekView()">Last Week</button>';
+  weekViewHtml += mm + ' ' + currentWeeklyViewDate.getDate() + ", " + yyyy + ' - ' + lastDayMm + ' ' + lastDateOfWeek.getDate() + ', ' + lastDayYyyy;
+  weekViewHtml += '<button id="right-arrow-weekly-bar" onclick="nextWeekView()">Next Week</button>';
+  weekViewHtml += '</p>';
+
+  $('#weekly-header-bar').html(weekViewHtml);
+  
+  weeklyTasks = new Map();
+
+  $('#tbl-list tbody').html('');
+
+  //Finds all tasks within the current week of the weekly view and adds them to a map of (day of month, task)
+  $(filteredData).each(function(index, task) {
+    taskDueDate = task.due;
+    
+    if (currentWeeklyViewDate <= taskDueDate && taskDueDate <= lastDateOfWeek) {
+      if (weeklyTasks.has(taskDueDate.getDate())) {
+        weeklyTasks.get(taskDueDate.getDate()).push(task);
+      }
+      else {
+        weeklyTasks.set(taskDueDate.getDate(), [task]);
+      }
+    }
+  });
+  
+  //Sorting the keys
+  keys = [];
+  for (var key of weeklyTasks.keys()) {
+    keys.push(key);
+  }
+
+  keys.sort(sortNumber);
+
+  //Adding the tasks of the week to the DOM
+  for (var key of keys) {
+    var tasksArr = weeklyTasks.get(key);
+
+    var firstTime = true;
+    for (var task of tasksArr) {
+      if (firstTime) {
+        $('#tbl-list tbody').append('<tr><td id="date-row" colspan="3">' + daysOfTheWeek[task.due.getDay()] + ', ' + monthsOfTheYear[task.due.getMonth()] + ' ' + task.due.getDate() + '</td></tr>');
+        firstTime = false;
+      }
+
+       var tr = $('<tr>');
+      $(tr).append('<td>'+task.name+'</td>');
+      $(tr).append('<td>'+getReadableDate(task.due)+'</td>');
+      $(tr).append('<td>'+task.priority+'</td>');
+      $('#tbl-list tbody').append(tr);
+    }
+  }
+
+}
+
+function sortNumber(a,b) {
+    return a - b;
 }
--- data.js
+++ data.js
@@ -40,5 +40,47 @@ var data = [
 		"due":new Date( 1418319900000),
 		"priority": "Medium",
 		"list": "General"
-	}
+	},
+	{
+		"name": "Math 313 7.2 HW",
+		"due": new Date("December 4, 2014 11:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "English 316 Research Report Draft",
+		"due": new Date("December 5, 2014 11:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "CS 142 Bank Accounts",
+		"due": new Date("December 6, 2014 11:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "Hang out with Zuck",
+		"due": new Date("December 14, 2014 11:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "Party right before finals",
+		"due": new Date("December 15, 2014 11:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "Fly to Mexico",
+		"due": new Date("December 16, 2014 11:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
+	{
+		"name": "Something due today",
+		"due": new Date("December 7, 2014 1:13:00"),
+		"priority": "High",
+		"list": "CS256"
+	},
 ];
